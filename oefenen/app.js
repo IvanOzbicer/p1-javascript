9a139499e5231ea1c412e879e6a0676ea1e0aac8
IvanOzbicer <9019539@student.zadkine.nl>
2022-09-15 14:56:18 +0200

    Let const answer

--- oefenen/app.js
+++ oefenen/app.js
@@ -9,3 +9,25 @@ document.write(myName);
 document.write(myAge);
 document.write(diploma);
 document.write 
+
+
+let a = 5 * 3;
+document.write(a)
+
+let num1 = 70;
+let num2 = 69;
+
+let answer = num1 + num2;
+console.log(answer)
+
+
+let answer1 = num1 - num2;
+console.log(answer1)
+
+let answer2 = num1 / num2;
+console.log(answer2)
+
+let answer3 = num1 * num2;
+console.log(answer3)
+
+
